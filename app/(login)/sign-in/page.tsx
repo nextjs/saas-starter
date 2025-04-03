eac740efdde68f2ee1683b32f8f796216ac747f1
imkrish7 <singhkrishk07@gmail.com>
2025-04-03 23:06:26 +0530

    redirection on login and signup page has been added

--- app/(login)/sign-in/page.tsx
+++ app/(login)/sign-in/page.tsx
@@ -1,7 +1,13 @@
-import { Suspense } from 'react';
-import { Login } from '../login';
+import { Suspense } from "react";
+import { Login } from "../login";
+import { getSession } from "@/lib/auth/session";
+import { redirect } from "next/navigation";
 
-export default function SignInPage() {
+export default async function SignInPage() {
+  const userSession = await getSession();
+  if (userSession) {
+    redirect("/dashboard");
+  }
   return (
     <Suspense>
       <Login mode="signin" />
