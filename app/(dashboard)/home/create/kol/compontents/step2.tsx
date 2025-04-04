2918cffcb455df7429b956fef5ad8a364a095713
Milhous <milhous.zhang@gmail.com>
2025-04-04 19:43:35 +0800

    add: create kol agent.

--- app/(dashboard)/home/create/kol/compontents/step2.tsx
+++ app/(dashboard)/home/create/kol/compontents/step2.tsx
@@ -56,7 +56,7 @@ export default function StepOne() {
         >
           <FormField
             control={form.control}
-            rules={{ required: "Ability is required" }} 
+            rules={{ required: "Ability is required" }}
             name="ability"
             render={({ field }) => (
               <FormItem>
