5f75ed971fc3d1ee50de637b4d527f62c14cb31f
Milhous <milhous.zhang@gmail.com>
2025-04-06 11:05:34 +0800

    update avatar image. update: split code.

---
+++ app/(dashboard)/home/create/kol/compontents/preview-post.tsx
@@ -0,0 +1,3 @@
+export default function PreviewPost() {
+  return <div className="bg-background rounded-md px-4 py-2">PreviewPost</div>;
+}
--- app/(dashboard)/home/create/kol/compontents/preview-step3.tsx
+++ app/(dashboard)/home/create/kol/compontents/preview-step3.tsx
@@ -1,30 +1,23 @@
 import { LoaderCircle } from "lucide-react";
 
+import PreviewLoader from "./preview-loader";
+import PreviewThinking from "./preview-thinking";
+
+const texts = [
+  "CZ Agent互动的用户，喜欢Layer2和web3相关的，通过X 推特平台进行，搜索名称和描述，以及发文内容，有关于Layer2和web3的进行查找，并通过热度和时间进行排序，找到热度最高的帖子进互动和转发。此类帖子用户群体比较多，系统默认媒体转发10篇，评论10次。开始执行程序",
+];
+
 export default function PreviewStepThree() {
   return (
     <div className="px-4 space-y-4 text-md">
-      <div className="bg-background rounded-md px-4 py-2 flex items-center space-x-1">
-        <LoaderCircle className="w-4 h-4 animate-spin" />
-        <span>思考中…(用时22秒)</span>
-      </div>
-      <div className="space-y-4 border-l-2 border-secondary pl-4">
-        <p>
-          CZ Agent互动的用户，喜欢Layer2和web3相关的，通过X
-          推特平台进行，搜索名称和描述，以及发文内容，有关于Layer2和web3的进行查找，并通过热度和时间进行排序，找到热度最高的帖子进互动和转发。此类帖子用户群体比较多，系统默认媒体转发10篇，评论10次。开始执行程序。。。。
-        </p>
-      </div>
+      <PreviewLoader text="思考中" time={22} />
+      <PreviewThinking texts={texts} />
       <div className="bg-background rounded-md px-4 py-2 flex items-center space-x-1">
         <LoaderCircle className="w-4 h-4 animate-spin" />
         <span>转推中…(用时10秒) 1/2</span>
       </div>
-      <div className="bg-background rounded-md px-4 py-2 flex items-center space-x-1">
-        <LoaderCircle className="w-4 h-4 animate-spin" />
-        <span>转推中…(用时10秒) 2/2</span>
-      </div>
-      <div className="bg-background rounded-md px-4 py-2 flex items-center space-x-1">
-        <LoaderCircle className="w-4 h-4 animate-spin" />
-        <span>评论中…(用时10秒) 1/2</span>
-      </div>
+      <PreviewLoader text="转推中" time={10} progress="1/2" />
+      <PreviewLoader text="评论中" time={10} progress="1/2" />
     </div>
   );
 }
