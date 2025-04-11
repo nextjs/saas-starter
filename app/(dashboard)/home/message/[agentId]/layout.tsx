9a117f85e8677459e4ea60ed11f33abfe267af14
Milhous <milhous.zhang@gmail.com>
2025-04-11 16:24:13 +0800

    add: mesaage UI.

---
+++ app/(dashboard)/home/message/[agentId]/layout.tsx
@@ -0,0 +1,24 @@
+import TwitterView from "@/app/components/home/TwitterView";
+import { Button } from "@/components/ui/button";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { ChevronLeft, Zap } from "lucide-react";
+import Link from "next/link";
+import React from "react";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="w-full h-full overflow-hidden flex">
+      <div className="w-full h-full overflow-hidden flex flex-col flex-1 box-border p-2 md:p-4 lg:p-6">
+        <div className="w-full flex items-center justify-between shadow-[0_10px_10px_20px_rgba(251,249,250,1)] pb-4">
+          <Link href="/home">
+            <div className="flex items-center gap-2">
+              <ChevronLeft className="w-6 h-6" />
+              <h1 className="text-base font-bold">Message</h1>
+            </div>
+          </Link>
+        </div>
+        <div className="w-full flex-1 overflow-auto">{children}</div>
+      </div>
+    </div>
+  );
+}
---
+++ app/(dashboard)/home/message/[agentId]/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function page() {
+  return (
+    <div className="w-full h-auto text-primary text-center py-12">
+      <div className="p-4 md:p-8 w-full h-full">111</div>
+    </div>
+  );
+}
