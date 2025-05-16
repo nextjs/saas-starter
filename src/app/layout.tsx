23771bae9268d1f946ebc80f25a9a06c69c349e9
matheus <matheusvieiracoelho@gmail.com>
2025-05-16 17:22:07 -0300

    Update layout.tsx to suppress hydration warning in HTML tag
    - Modified the HTML tag in layout.tsx to include suppressHydrationWarning for improved rendering behavior in React applications.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={`${inter.className}`}>
+    <html lang="en" className={`${inter.className}`} suppressHydrationWarning>
       <body className="min-h-[100dvh] bg-background text-foreground">
         <SWRConfig
           value={{
